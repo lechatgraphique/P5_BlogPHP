40fd5e1d8a4c5a36bb113677b59591f7566b7b54
Bastien Clement <hello@lechatgraphique.fr>
2020-02-10 16:39:09 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Configuration pour la connexion à la base de données
+ */
+define('DB_HOST', 'localhost');
+
+define('DB_NAME', 'p5zkmp_blog');
+
+define('DB_USER', 'lechatgraphique');
+
+define('DB_PASS', 'lechatgraphique');
