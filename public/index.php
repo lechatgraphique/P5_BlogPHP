217324df6f2ec565e1e156ba6eeb8cda34f042de
Bastien Clement <hello@lechatgraphique.fr>
2020-02-11 12:43:58 +0100

    Update template and minor update

--- public/index.php
+++ public/index.php
@@ -1,3 +1,41 @@
 <?php
 
-require "vendor/autoload.php";
+require "../vendor/autoload.php";
+
+$whoops = new \Whoops\Run;
+
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+$loader = new \Twig\Loader\FilesystemLoader('../templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+$router = new AltoRouter();
+
+$router->map( 'GET', '/', function() use ($twig) {
+
+    echo $twig->render('frontend/home/index.twig', []);
+});
+
+$router->map( 'GET', '/posts', function() use ($twig) {
+    echo $twig->render('frontend/post/index.twig', []);
+});
+
+$router->map( 'GET', '/post', function() use ($twig) {
+    echo $twig->render('frontend/post/show.twig', []);
+});
+
+$router->map( 'GET', '/contact', function() use ($twig) {
+    echo $twig->render('frontend/contact/index.twig', []);
+});
+
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+    call_user_func_array( $match['target'], $match['params'] );
+} else {
+    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -68,7 +68,6 @@ class Post
         return $this->id;
     }
 
-
     /**
      * @return string
      */
