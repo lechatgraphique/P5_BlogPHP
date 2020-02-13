6b600e603d4e7bfd8980df60dd5b3e7429c0590c
Bastien Clement <hello@lechatgraphique.fr>
2020-02-13 17:46:05 +0100

    Add templates admin & Update template frontend

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Render\Twig;
+
+class AdminController
+{
+    private $twig;
+
+    public function __construct()
+    {
+        $this->twig = new Twig();
+    }
+
+    public function index()
+    {
+        echo $this->twig->getTwig()->render('backend/dashboard/index.twig', []);
+    }
+}
