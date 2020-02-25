e01934b7565e48e408411393ffa7bed0135b13bd
Bastien Clement <hello@lechatgraphique.fr>
2020-02-25 21:06:42 +0100

    login user block disabled

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -20,19 +20,29 @@ class ConnexionController
 
     public function index()
     {
+        $flash = SessionFlash::renderSessionFlash();
+
         if(Auth::user()) {
             header("Location: /");
         } else {
-            echo $this->twig->getTwig()->render('frontend/login/index.twig', []);
+            echo $this->twig->getTwig()->render('frontend/login/index.twig', [
+                'flash' => $flash
+            ]);
         }
     }
 
     public function login(array $params)
     {
 
-
         $userRepository = new UserRepository();
         $user = $userRepository->findUser($params['post']['username']);
+
+        if($user->getDisabled() == 1) {
+            SessionFlash::sessionFlash("danger", "Votre compte a été suspendu.");
+            header("Location: /connexion");
+            return;
+        }
+
 
         if($user && password_verify($params['post']['password'], $user->getPassword())) {
             Auth::login($user);
